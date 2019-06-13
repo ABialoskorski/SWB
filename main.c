e12b8427516460ae7acc3303e186edecaec94553
Artur Białoskórski <bialoskorski.artur@gmail.com>
2019-06-13 22:16:39 +0200

    new functions

--- main.c
+++ main.c
@@ -34,16 +34,17 @@ char getoptChar(int argc, char **argv)
 int main(int argc, char **argv)
 {
   int a, b, c, wynik;
-  printf("a - POLE PROSTOKATA | b - POLE TRÓJKATA | c - ls -l \n");
+  // printf("a = POLE PROSTOKATA | b = POLE TRÓJKATA | c = ls -al | d = pwd \n");
   char liczba = getoptChar(argc, argv), command[100], opcja;
 
   char folder[100];
   char mk[100];
   if (liczba == 'a')
   {
-    printf("WYBIERZ ZADANIE: ");
+    printf("a = POLE PROSTOKATA | b = POLE TRÓJKATA \n");
+    printf("WYBIERZ ZADANIE a/b: \n");
     scanf("%s", &opcja);
-    printf("PODAJ LICZBY: ");
+    printf("PODAJ 2 LICZBY: ");
     scanf("%i %i", &a, &b);
 
     switch (opcja)
@@ -56,10 +57,6 @@ int main(int argc, char **argv)
       printf("POLE TRÓJKATA: ");
       wynik = trojkat(a, b);
       break;
-    case 'c':
-      strcpy(command, "ls -l");
-      system(command);
-      return 0;
     default:
       printf("%s\n", "COS POSZLO NIE TAK!");
       return -1;
@@ -67,12 +64,17 @@ int main(int argc, char **argv)
   }
   if (liczba == 'b')
   {
-    printf("WYBIERZ C: ");
+    printf("c = ls -al | d = pwd \n");
+    printf("WYBIERZ ZADANIE c/d:  \n");
     scanf("%s", &opcja);
     switch (opcja)
     {
     case 'c':
-      strcpy(command, "ls -l");
+      strcpy(command, "ls -al");
+      system(command);
+      return 0;
+    case 'd':
+      strcpy(command, "pwd");
       system(command);
       return 0;
     default:
